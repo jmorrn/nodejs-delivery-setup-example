bd5e0ad9c79490ee639d799f35a6b51f70df2933
Jeanluca-Moren0 <jeanlucam12@gmail.com>
2024-02-27 15:45:43 -0300

    chore: example types in js

---
+++ src/types.js
@@ -0,0 +1,12 @@
+/**
+ * @typedef {Object} IncomingUser
+ * @prop {string} name
+ * @prop {string} email
+ * @prop {string} password
+ */
+
+/**
+ * @typedef {Object} OutcomingUser
+ * @prop {string} name
+ * @prop {string} email
+ */
